009f22498acd48ff9d1532b689823d9fa80f5dfa
pourya <pourya_1999@yahoo.com>
2022-09-30 03:25:13 +0330

    'unet_model'

--- model.py
+++ model.py
@@ -7,10 +7,10 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False)
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False)
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
@@ -32,3 +32,14 @@ class UNet(nn.Module):
             in_channels = feature
 
         #! up part of unet:
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    features, feature, kernel_size=2, stride=2,
+                )
+            )
+            self.ups.append(DoubleConv(feature*2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+        
